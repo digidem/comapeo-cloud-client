394b60568f6bc4de5b85da03cc19e35b7af75753
Evan Hahn <me@evanhahn.com>
2024-06-19 18:02:54 +0000

    chore: create a MapeoManager

--- src/server.js
+++ src/server.js
@@ -1,4 +1,32 @@
+import { MapeoManager } from '@mapeo/core'
+import Fastify from 'fastify'
+import RAM from 'random-access-memory'
+
+import * as path from 'node:path'
+
+const migrationsFolderPath = new URL(
+  '../node_modules/@mapeo/core/drizzle',
+  import.meta.url,
+).pathname
+
 export class Server {
+  #fastify = Fastify()
+  #mapeoManager
+
+  constructor() {
+    this.#mapeoManager = new MapeoManager({
+      // TODO: Don't hard-code this
+      rootKey: Buffer.from('mWZ5Qi0oay0KInZ9F/pMCQ==', 'base64'),
+      // TODO: Save database to disk
+      dbFolder: ':memory:',
+      clientMigrationsFolder: path.join(migrationsFolderPath, 'client'),
+      projectMigrationsFolder: path.join(migrationsFolderPath, 'project'),
+      // TODO: Save data to disk
+      coreStorage: () => new RAM(),
+      fastify: this.#fastify,
+    })
+  }
+
   /**
    * @param {object} options
    * @param {number} options.port
@@ -7,6 +35,7 @@ export class Server {
   listen({ port }) {
     // TODO
     console.log('Starting server on port ' + port)
+    console.log(this.#mapeoManager)
     return Promise.resolve()
   }
 
